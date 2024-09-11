4bfac3fb40291d88daa1d1b0a8decc9c4abd0229
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-09-11 10:04:32 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const faveNumber1 = 19; 
+
+const faveNumber2 = 13; 
+
+const sum = faveNumber1 + faveNumber2; 
+
+const difference = faveNumber1 - faveNumber2; 
+
+const product = faveNumber1 * faveNumber2;
+
+const quotient = faveNumber1 / faveNumber2; 
+
+const remainder = faveNumber1 % faveNumber2; 
+
+const power = faveNumber1 ** faveNumber2 
+
+console.log(sum); 
+
+console.log(difference); 
+
+console.log(product); 
+
+console.log(quotient); 
+
+console.log(remainder); 
+
+console.log(power); 
